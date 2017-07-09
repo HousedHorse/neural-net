4cdc80e52893e07d1689d5c2fc148223e3fb1ac2
HousedHorse <william@wfindlay.com>
2017-07-08 20:20:46 -0400

    basic prototype for inputarray

--- InputArray.h
+++ InputArray.h
@@ -1,5 +1,13 @@
 #ifndef INPUT_ARRAY_H
 #define INPUT_ARRAY_H
 
+template <class T> class InputArray{
+public:
+  int getSize();
+  int add(T entry);
+  T* data;
+protected:
+  int size;
+};
 
 #endif
