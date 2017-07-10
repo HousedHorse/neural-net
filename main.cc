e54345582de211a0b5f237d0803c178a38253826
housedhorse <william@wfindlay.com>
2017-07-10 22:11:58 +0100

    removed some test code in main

--- main.cc
+++ main.cc
@@ -6,9 +6,5 @@ int main(int argc, char* argv[]){
   //seed the random number generator
   srand(time(NULL));
   
-  Perceptron* p = new Perceptron;
-  vector<int>* foo;
   cout << rand();
- 
-  //p->process(foo);
 }
