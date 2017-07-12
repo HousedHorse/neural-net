ff9d71cbf5b365b3941e6d80a56f95253e64d276
housedhorse <william@wfindlay.com>
2017-07-12 05:04:37 +0100

    starting to mess around with dynamic array

--- DynamicArray.h
+++ DynamicArray.h
@@ -1,3 +1,16 @@
-class DynamicArray : private vector{
-  
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+
+#include "defs.h"
+#include 
+
+template <class T> class DynamicArray{
+public:
+  T push();
+  T pop();
+  T operator[];
+private:
+  vector<T> data;
 };
+
+#endif
