772a2370aa8daffa3a892718797693ef20f311a2
housedhorse <william@wfindlay.com>
2017-07-12 03:04:53 +0100

    bug fix

--- Point.cc
+++ Point.cc
@@ -11,6 +11,8 @@ int Point::getSize(){ return size; }
 int Point::push(int n){data[size] = n; size++; return n; }
 
 int Point::pop(){
+  if(size == 0)
+    return NULL;
   size--;
   int n = data[size];
   return n;
