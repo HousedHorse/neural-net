db5627bd42fc7ac78aca3c261a92bb9fe3dbef1b
HousedHorse <william@wfindlay.com>
2017-07-12 15:12:39 -0400

    template class for dynamic arrays

--- DynamicArray.h
+++ DynamicArray.h
@@ -1,16 +1,54 @@
 #ifndef DYNAMIC_ARRAY_H
 #define DYNAMIC_ARRAY_H
 
-#include "defs.h"
-#include 
+#include <vector>
+#include <stdexcept>
+#include <iostream>
 
-template <class T> class DynamicArray{
+template <class T>
+class DynamicArray{
 public:
-  T push();
+  T push(T n);
   T pop();
-  T operator[];
+  int getSize();
+  T& operator[](const int index);
 private:
-  vector<T> data;
+  std::vector<T> data;
 };
+
+template <class T>
+T DynamicArray<T>::push(T n){
+  data.push_back(n);
+  return n;
+}
+
+template <class T>
+T DynamicArray<T>::pop(){
+  //check to see if data vector is empty
+  if(data.empty())
+    return 0;
+  //store the value in the last element
+  T n = data.back();
+  //pop the last element
+  data.pop_back();
+  //return the stored value
+  return n;
+}
+
+template <class T>
+T& DynamicArray<T>::operator[](const int index){
+  try{
+    return data.at(index);
+  }
+  catch (const std::out_of_range& e) {
+    std::cerr << "ERROR: OUT OF RANGE\n";
+    throw 0;
+  }
+}
+
+template <class T>
+int DynamicArray<T>::getSize(){
+  return data.size();
+}
 
 #endif
