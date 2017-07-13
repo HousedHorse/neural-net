464af5e0c1d67a65a2abb9f417bffb561f50dc49
HousedHorse <william@wfindlay.com>
2017-07-13 15:58:03 -0400

    testing

--- main.cc
+++ main.cc
@@ -7,15 +7,15 @@ int main(int argc, char* argv[]){
   srand(time(NULL));
   
   Perceptron* perc = new Perceptron[1000];
-  Point* p = new Point[20];
+  Point* p = new Point[40];
   
-  for(int i = 0; i < 20; i++){
+  for(int i = 0; i < 40; i++){
     p[i] = Point("random");
   }
   
   for(int j = 0; j < 1000; j++){
     cout << "Perceptron #" << perc[j].getId() << ": " << endl;
-    for(int i = 0; i < 20; i++){
+    for(int i = 0; i < 40; i++){
       cout << "Processed value of point #" << p[i].getId()
       << " is: " << perc[j].process(p[i]) << endl;
     }
