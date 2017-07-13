a5d0884718cf5a2ee7b78a535a3301af9e81a404
HousedHorse <william@wfindlay.com>
2017-07-13 16:29:46 -0400

    testing

--- main.cc
+++ main.cc
@@ -1,24 +1,21 @@
 #include "defs.h"
 #include "Perceptron.h"
 #include "Point.h"
+#include "DynamicArray.h"
 
 int main(int argc, char* argv[]){
   //seed the random number generator
   srand(time(NULL));
   
-  Perceptron* perc = new Perceptron[1000];
-  Point* p = new Point[40];
+  Perceptron perc = Perceptron();
+  DynamicArray<Point> points;
   
-  for(int i = 0; i < 40; i++){
-    p[i] = Point("random");
+  for(int i = 0; i < 20; i++){
+    points.push(Point("random"));
   }
   
-  for(int j = 0; j < 1000; j++){
-    cout << "Perceptron #" << perc[j].getId() << ": " << endl;
-    for(int i = 0; i < 40; i++){
-      cout << "Processed value of point #" << p[i].getId()
-      << " is: " << perc[j].process(p[i]) << endl;
-    }
-    cout << endl;
+  for(int i = 0; i < points.getSize(); i++){
+    cout << points[i].getId() << endl;
   }
+ 
 }
